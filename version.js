574715f6381a137a788aefc7796e3b551415459d
Choice575 <Mixail576@yandex.ru>
2026-08-04 18:30:28 +0300

    chore(release): v14.0.0

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 // Single source of truth for the application and offline cache version.
-self.IPMAX_VERSION = '13.7.0';
+self.IPMAX_VERSION = '14.0.0';
 self.IPMAX_CACHE_PREFIX = 'ipmax-v';
 self.IPMAX_CACHE_NAME = self.IPMAX_CACHE_PREFIX + self.IPMAX_VERSION;
